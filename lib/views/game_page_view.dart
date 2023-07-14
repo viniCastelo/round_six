ab1809e3ce6c697b09ced9b1da042e8d77ab4672
Vinícius Castelo <vkstello@gmail.com>
2023-07-14 20:42:58 -0300

    Criando a animação das cartas.

--- lib/views/game_page_view.dart
+++ lib/views/game_page_view.dart
@@ -1,6 +1,8 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first, prefer_const_literals_to_create_immutables
 // ignore_for_file: prefer_const_constructors, sort_child_properties_last
 
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 import 'package:round_six/models/enums/constants.dart';
@@ -28,6 +30,15 @@ class GamePageView extends StatelessWidget {
     return 4;
   }
 
+  int getRandomNumber() {
+    Random random = Random();
+    int randomNumber = random.nextInt(15);
+    if (randomNumber == 0) {
+      return 1;
+    }
+    return randomNumber;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -41,7 +52,10 @@ class GamePageView extends StatelessWidget {
           padding: EdgeInsets.all(24.0),
           children: List.generate(
             level,
-            (index) => CardGameWidget(modo: modo),
+            (index) => CardGameWidget(
+              modo: modo,
+              opcao: getRandomNumber(),
+            ),
           ),
         ),
       ),
--- lib/widgets/card_game_widget.dart
+++ lib/widgets/card_game_widget.dart
@@ -1,35 +1,99 @@
-// ignore_for_file: public_member_api_docs, sort_constructors_first
+// ignore_for_file: public_member_api_docs, sort_constructors_first, prefer_const_constructors
+import 'dart:async';
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 import 'package:round_six/models/enums/constants.dart';
 import 'package:round_six/theme/theme.dart';
 
-class CardGameWidget extends StatelessWidget {
+class CardGameWidget extends StatefulWidget {
   const CardGameWidget({
     Key? key,
     required this.modo,
+    required this.opcao,
   }) : super(key: key);
 
   final Modo modo;
+  final int opcao;
+
+  @override
+  State<CardGameWidget> createState() => _CardGameWidgetState();
+}
+
+class _CardGameWidgetState extends State<CardGameWidget>
+    with SingleTickerProviderStateMixin {
+  late final AnimationController animation;
+
+  @override
+  void initState() {
+    super.initState();
+    animation = AnimationController(
+      vsync: this,
+      duration: Duration(milliseconds: 400),
+    );
+  }
+
+  @override
+  void dispose() {
+    animation.dispose();
+    super.dispose();
+  }
+
+  flipCard() {
+    if (!animation.isAnimating) {
+      animation.forward();
+      Timer(
+        Duration(seconds: 2),
+        () {
+          animation.reverse();
+        },
+      );
+    }
+  }
+
+  AssetImage getImage(double angle) {
+    if (angle > 0.5 * pi) {
+      return AssetImage('images/${widget.opcao.toString()}.png');
+    } else {
+      return widget.modo == Modo.normal
+          ? AssetImage('images/card_normal.png')
+          : AssetImage('images/card_round.png');
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(10),
-        border: Border.all(
-          color: modo == Modo.normal ? Colors.white : RoundSixTheme.mainColor,
-          width: 3.0,
-        ),
-      ),
-      child: ClipRRect(
-        borderRadius: BorderRadius.circular(10),
-        child: Image.asset(
-          modo == Modo.normal
-              ? 'images/card_normal.png'
-              : 'images/card_round.png',
-        ),
-      ),
+    return AnimatedBuilder(
+      animation: animation,
+      builder: (context, child) {
+        final angle = animation.value * pi;
+        final transform = Matrix4.identity()
+          ..setEntry(3, 2, 0.002)
+          ..rotateY(angle);
+        return GestureDetector(
+          onTap: () => flipCard(),
+          child: Transform(
+            transform: transform,
+            alignment: Alignment.center,
+            child: Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10),
+                border: Border.all(
+                  color: widget.modo == Modo.normal
+                      ? Colors.white
+                      : RoundSixTheme.mainColor,
+                  width: 2.0,
+                ),
+                image: DecorationImage(
+                  fit: BoxFit.cover,
+                  image: getImage(angle),
+                ),
+              ),
+            ),
+          ),
+        );
+      },
     );
   }
 }
